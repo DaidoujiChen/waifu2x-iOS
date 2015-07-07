9a25d5ba45af9b97eb115f78721979b4f5801468
DaidoujiChen <daidoujichen@gmail.com>
2015-07-07 11:45:12 +0800

    Initial Commit

---
+++ waifu2x-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waifu2x-iOS
+//
+//  Created by DaidoujiChen on 2015/7/7.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waifu2x-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waifu2x-iOS
+//
+//  Created by DaidoujiChen on 2015/7/7.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
